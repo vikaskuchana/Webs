7b49d9e8a40e748dbe721252408caa1f2aa1710c
vikaskuchana <122292759+vikaskuchana@users.noreply.github.com>
2023-03-08 17:19:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Get the element with id "name"
+const nameElement = document.getElementById("name");
+
+// Retrieve the text content of the element
+const name = nameElement.textContent;
+var e="Good"
+// Log the name to the console
+console.log(e);
+console.log(name);
